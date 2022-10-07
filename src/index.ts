4cd2f118ff16e5ddb55c3a92b4323eb598eff019
Mike Thompson <mikeylikes23@gmail.com>
2022-10-07 07:57:24 -0600

    Incremented version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import ProjectGenerator from './ProjectGenerator'
 import DependencyChecker from './DependencyChecker'
 import chalk from 'chalk'
 
-const version = '0.0.13'
+const version = '0.0.14'
 const {performance} = require('perf_hooks')
 
 const program = new Command()
